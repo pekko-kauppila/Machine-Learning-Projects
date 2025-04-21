5d9a9adb9d438d9c2e71f037590d5004109d626e
Pekko Kauppila <pekko.kauppila@hotmail.com>
2025-04-21 21:52:20 +0300

    Cleaned up

--- check_notebook_pep8_compliance.py
+++ check_notebook_pep8_compliance.py
@@ -8,7 +8,7 @@ def check_pep8_compliance(path_to_notebook):
         return
 
     print(f"PEP8 check for: {path_to_notebook}")
-    result = subprocess.run(['nbqa', 'flake8', path_to_notebook, '--max-line-length=79'], capture_output=True, text=True)
+    result = subprocess.run(['nbqa', 'flake8', path_to_notebook, '--max-line-length=120'], capture_output=True, text=True)
 
     if result.returncode == 0:
         print('PEP8 compliance check passed!')
